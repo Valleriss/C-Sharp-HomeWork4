f717cb4936f41f41c6cc5a17471d7ac259a9339a
LeraLukashova <lerashilova07@gmail.com>
2023-04-08 22:04:31 +0300

    Решение задачи 25

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16 */
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine($"{a} ^ {b} = {Exponentiate(a, b)}");
+
+
+int Exponentiate(int a, int b)
+{
+    int exp = a;
+    for (int i = 1; i < b; i++)
+    {
+        exp *= a;
+    }
+    return exp;
+}
